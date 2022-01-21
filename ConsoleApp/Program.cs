04f37887f20a4d11776ef905791fd247104b214d
NepEvg <eneputina@inbox.ru>
2022-01-21 19:14:55 +0500

    twelfth project

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("введите A: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("введите B: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("выберите операцию [ + | - | * | / ] ");
+            string ans = Console.ReadLine();
+            switch (ans)
+            {
+                case "+":
+
+                    break;
+                case "-":
+
+                    break;
+                case "*":
+
+                    break;
+                case "/":
+
+                    break;
+                default:
+                    Console.WriteLine("такой операции нет");
+                    break;
+            }
+        }
+    }
+}
